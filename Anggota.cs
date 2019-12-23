56f48a91114ef71dbc298bef300360cb02abf7dc
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-23 17:28:50 +0700

    Add project files.

---
+++ Anggota.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor__CS
+{
+    class Anggota
+    {
+        public string Nama = "Rizky Khapidsyah";
+        public int Umur = 28;
+
+        public Anggota()
+        {
+            Console.WriteLine("Halloo, Nama saya Adalah: {0} ", Nama);
+        }
+
+        public Anggota(string NamaParam)
+        {
+            Nama = NamaParam;
+            Console.WriteLine("Nama Panggilan Saya Adalah: {0}", Nama);
+        }
+
+        public Anggota(int UmurParam)
+        {
+            Umur = UmurParam;
+            Console.WriteLine("Usia Saya Adalah: {0}", Umur + " Tahun");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Constructor__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] SInput = new String[2];
+            int umurMain;
+
+            Anggota[] K = new Anggota[3];
+
+            //Tampilan Awal
+            K[0] = new Anggota();
+            K[1] = new Anggota("Rizky");
+            K[2] = new Anggota(102);
+
+
+            Console.Write("\n\nNama Panggilan Anda : "); SInput[0] = Console.ReadLine();
+            Console.Write("Berapa Umur Anda?   : "); umurMain = Convert.ToInt32(SInput[1] = Console.ReadLine());
+            Console.WriteLine("\n");
+
+            K[1] = new Anggota(SInput[0]);
+            K[2] = new Anggota(umurMain);
+
+            Console.ReadLine();
+        }
+    }
+}
